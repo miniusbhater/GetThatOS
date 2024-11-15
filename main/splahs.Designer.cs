a97e7e936080c2aa74cc170b10c42235031701a4
monoxide <numpad0000@gmail.com>
2024-11-15 21:58:33 +0000

    Add files via upload

--- main/splahs.Designer.cs
+++ main/splahs.Designer.cs
@@ -71,9 +71,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(323, 399);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.Size = new System.Drawing.Size(45, 16);
             this.label3.TabIndex = 3;
-            this.label3.Text = "0.1.9";
+            this.label3.Text = "0.1.9-1";
             // 
             // splahs
             // 
