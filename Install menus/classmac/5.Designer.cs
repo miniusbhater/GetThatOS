fb945556790a41ba5482d217bcbfc501ccde8db0
CO <numpad0000@gmail.com>
2024-11-22 20:21:50 +0000

    Add files via upload

--- Install menus/classmac/5.Designer.cs
+++ Install menus/classmac/5.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace GetThatOS.Install_menus.Windows
+﻿namespace GetThatOS.Install_menus.classmac
 {
-    partial class windows_3
+    partial class _5
     {
         /// <summary>
         /// Required designer variable.
@@ -28,39 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(windows_3));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_5));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Location = new System.Drawing.Point(69, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(309, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Select your version and download directory";
+            this.label1.Size = new System.Drawing.Size(190, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Select download directory";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 154);
+            this.textBox1.Location = new System.Drawing.Point(124, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(199, 20);
-            this.textBox1.TabIndex = 9;
+            this.textBox1.TabIndex = 17;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(53, 152);
+            this.button5.Location = new System.Drawing.Point(67, 32);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(51, 23);
-            this.button5.TabIndex = 11;
+            this.button5.TabIndex = 18;
             this.button5.Text = "browse";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -69,62 +68,49 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1, 157);
+            this.label3.Location = new System.Drawing.Point(9, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 13);
-            this.label3.TabIndex = 12;
+            this.label3.TabIndex = 21;
             this.label3.Text = "Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "5.x";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 32);
+            this.button1.Location = new System.Drawing.Point(107, 65);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 42);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "3.0";
+            this.button1.Size = new System.Drawing.Size(127, 30);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Download";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(166, 32);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 42);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "3.1";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(92, 80);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 42);
-            this.button3.TabIndex = 15;
-            this.button3.Text = "3.11";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // windows_3
+            // _5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 186);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(335, 107);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "windows_3";
+            this.Name = "_5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "windows_3";
-            this.Load += new System.EventHandler(this.windows_3_Load);
+            this.Text = "Get that OS";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,8 +122,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
---
+++ Install menus/classmac/5.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetThatOS.Install_menus.classmac
+{
+    public partial class _5 : Form
+    {
+        public _5()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mac = "https://github.com/breathemonoxide/diskimages/releases/download/5.0/System.Tools.1.img";
+            string directoryPath = textBox1.Text;
+            string fileName = "System.Tools.1.img";
+            string fullPath = Path.Combine(directoryPath, fileName);
+
+            button1.Enabled = false;
+            button1.Font = new Font(button1.Font.FontFamily, 9);
+
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.DownloadProgressChanged += (s, ev) =>
+                {
+                    button1.Text = $"Downloading... {ev.ProgressPercentage}%";
+                };
+
+                webclient.DownloadFileCompleted += (s, ev) =>
+                {
+                    button1.Text = "Download";
+                    button1.Enabled = true;
+                    button1.Font = new Font(button1.Font.FontFamily, 15.75f);
+                   
+                };
+
+                webclient.DownloadFileAsync(new Uri(mac), fullPath);
+
+                string mac1 = "https://github.com/breathemonoxide/diskimages/releases/download/5.0/System.Tools.2.img";
+                string directoryPath1 = textBox1.Text;
+                string fileName1 = "System.Tools.2.img";
+                string fullPath1 = Path.Combine(directoryPath1, fileName1);
+
+                button1.Enabled = false;
+                button1.Font = new Font(button1.Font.FontFamily, 9);
+
+                using (WebClient webclient1 = new WebClient())
+                {
+                    webclient1.DownloadProgressChanged += (s1, ev1) =>
+                    {
+                        button1.Text = $"Downloading... {ev1.ProgressPercentage}%";
+                    };
+
+                    webclient1.DownloadFileCompleted += (s1, ev) =>
+                    {
+                        button1.Text = "Download";
+                        button1.Enabled = true;
+                        button1.Font = new Font(button1.Font.FontFamily, 15.75f);
+                   
+                    };
+
+                    webclient1.DownloadFileAsync(new Uri(mac1), fullPath1);
+                }
+
+                string mac11 = "https://github.com/breathemonoxide/diskimages/releases/download/5.0/Utilities.1.img";
+                string directoryPath11 = textBox1.Text;
+                string fileName11 = "Utilities.1.img";
+                string fullPath11 = Path.Combine(directoryPath11, fileName11);
+
+                button1.Enabled = false;
+                button1.Font = new Font(button1.Font.FontFamily, 9);
+
+                using (WebClient webclient11 = new WebClient())
+                {
+                    webclient11.DownloadProgressChanged += (s11, ev11) =>
+                    {
+                        button1.Text = $"Downloading... {ev11.ProgressPercentage}%";
+                    };
+
+                    webclient11.DownloadFileCompleted += (s1, ev) =>
+                    {
+                        button1.Text = "Download";
+                        button1.Enabled = true;
+                        button1.Font = new Font(button1.Font.FontFamily, 15.75f);
+                       
+                    };
+
+                    webclient11.DownloadFileAsync(new Uri(mac11), fullPath11);
+
+                }
+            }
+
+            string mac111 = "https://github.com/breathemonoxide/diskimages/releases/download/5.0/Utilities.2.img";
+            string directoryPath111 = textBox1.Text;
+            string fileName111 = "Utilities.2.img";
+            string fullPath111 = Path.Combine(directoryPath111, fileName111);
+
+            button1.Enabled = false;
+            button1.Font = new Font(button1.Font.FontFamily, 9);
+
+            using (WebClient webclient111 = new WebClient())
+            {
+                webclient111.DownloadProgressChanged += (s111, ev111) =>
+                {
+                    button1.Text = $"Downloading... {ev111.ProgressPercentage}%";
+                };
+
+                webclient111.DownloadFileCompleted += (s111, ev111) =>
+                {
+                    button1.Text = "Download";
+                    button1.Enabled = true;
+                    button1.Font = new Font(button1.Font.FontFamily, 15.75f);
+                    MessageBox.Show("Download complete", "Status");
+                };
+
+                webclient111.DownloadFileAsync(new Uri(mac111), fullPath111);
+
+            }
+        }
+
+    }
+    }
+
+    
+
