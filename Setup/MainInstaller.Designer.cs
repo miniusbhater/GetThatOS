252d95c79938687b9482a781b499e2b59c588379
Rose <miniusbhater@gmail.com>
2026-04-07 20:11:11 +0100

    Add files via upload

---
+++ Setup/MainInstaller.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace GetThatOSSetup
+{
+    partial class MainInstaller
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(287, 47);
+            label1.TabIndex = 0;
+            label1.Text = "GetThatOS Setup";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(326, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 1;
+            label2.Text = "miniusbhater";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(356, 15);
+            label3.TabIndex = 2;
+            label3.Text = "GetThatOS Will Be Installed To: 'C:\\Program Files (x86)\\GetThatOS'\r\n";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(143, 124);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 55);
+            button1.TabIndex = 3;
+            button1.Text = "Install";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(326, 157);
+            button2.Name = "button2";
+            button2.Size = new Size(77, 22);
+            button2.TabIndex = 4;
+            button2.Text = "More";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(305, 41);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 15);
+            label4.TabIndex = 5;
+            label4.Text = "0.2.7";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(153, 106);
+            label5.Name = "label5";
+            label5.Size = new Size(116, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Installing Please wait";
+            label5.Visible = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(31, 106);
+            label6.Name = "label6";
+            label6.Size = new Size(363, 15);
+            label6.TabIndex = 7;
+            label6.Text = "Install Complete You May Launch GetThatOS From Your Start Menu\r\n";
+            label6.Visible = false;
+            // 
+            // MainInstaller
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(415, 191);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "MainInstaller";
+            Text = "Setup";
+            Load += MainInstaller_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Setup/MainInstaller.cs
@@ -0,0 +1,263 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using IWshRuntimeLibrary;
+
+namespace GetThatOSSetup
+{
+    public partial class MainInstaller : Form
+    {
+        public MainInstaller()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Visible = true;
+            label5.Text = ("Creating root folder");
+            string ProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            string CreateFolder = Path.Combine(ProgramFiles, "GetThatOS");
+            Directory.CreateDirectory(CreateFolder);
+            label5.Text = ("Installing executable");
+            string download = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS.exe";
+            string output = Path.Combine(CreateFolder, "GetThatOS.exe");
+
+            using (WebClient webclient = new WebClient())
+            {
+                webclient.DownloadFile(new Uri(download), output);
+            }
+            label5.Text = ("Installing repo shorcut");
+            string downloadurl = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/GetThatOS%20Repository.url";
+            string outputurl = Path.Combine(CreateFolder, "GetThatOS Repository.url");
+
+            using (WebClient webclienturl = new WebClient())
+            {
+                webclienturl.DownloadFile(new Uri(downloadurl), outputurl);
+            }
+
+            
+            label5.Text = ("Creating resources folder");
+            string ProgramFiles1 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            string CreateFolder1 = Path.Combine(ProgramFiles1, "GetThatOS", "LogosAndImages");
+            Directory.CreateDirectory(CreateFolder1);
+            label5.Text = ("Installing resources");
+            string download1 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/GetThatOS_logo.ico";
+            string output1 = Path.Combine(CreateFolder1, "GetThatOS_logo.ico");
+
+            using (WebClient webclient1 = new WebClient())
+            {
+                webclient1.DownloadFile(new Uri(download1), output1);
+            }
+
+            string download2 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/LinuxTux_logo.ico";
+            string output2 = Path.Combine(CreateFolder1, "LinuxTux_logo.ico");
+
+            using (WebClient webclient2 = new WebClient())
+            {
+                webclient2.DownloadFile(new Uri(download2), output2);
+            }
+
+            string download3 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/MacModern_logo.ico";
+            string output3 = Path.Combine(CreateFolder1, "MacModern_logo.ico");
+
+            using (WebClient webclient3 = new WebClient())
+            {
+                webclient3.DownloadFile(new Uri(download3), output3);
+            }
+
+            string download4 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/MacFlat_logo.ico";
+            string output4 = Path.Combine(CreateFolder1, "MacFlat_logo.ico");
+
+            using (WebClient webclient4 = new WebClient())
+            {
+                webclient4.DownloadFile(new Uri(download4), output4);
+            }
+
+            string download5 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/MacOSX_logo.ico";
+            string output5 = Path.Combine(CreateFolder1, "MacOSX_logo.ico");
+
+            using (WebClient webclient5 = new WebClient())
+            {
+                webclient5.DownloadFile(new Uri(download5), output5);
+            }
+
+            string download6 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/MacPreOSX_logo.ico";
+            string output6 = Path.Combine(CreateFolder1, "MacPreOSX_logo.ico");
+
+            using (WebClient webclient6 = new WebClient())
+            {
+                webclient6.DownloadFile(new Uri(download6), output6);
+            }
+
+            string download7 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows11_logo.ico";
+            string output7 = Path.Combine(CreateFolder1, "Windows11_logo.ico");
+
+            using (WebClient webclient7 = new WebClient())
+            {
+                webclient7.DownloadFile(new Uri(download7), output7);
+            }
+
+            string download8 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows10_logo.ico";
+            string output8 = Path.Combine(CreateFolder1, "Windows10_logo.ico");
+
+            using (WebClient webclient8 = new WebClient())
+            {
+                webclient8.DownloadFile(new Uri(download8), output8);
+            }
+
+            string download9 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows8_logo.ico";
+            string output9 = Path.Combine(CreateFolder1, "Windows8_logo.ico");
+
+            using (WebClient webclient9 = new WebClient())
+            {
+                webclient9.DownloadFile(new Uri(download9), output9);
+            }
+
+            string download10 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows7_logo.ico";
+            string output10 = Path.Combine(CreateFolder1, "Windows7_logo.ico");
+
+            using (WebClient webclient10 = new WebClient())
+            {
+                webclient10.DownloadFile(new Uri(download10), output10);
+            }
+
+            string download11 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/WindowsVista_logo.ico";
+            string output11 = Path.Combine(CreateFolder1, "WindowsVista_logo.ico");
+
+            using (WebClient webclient11 = new WebClient())
+            {
+                webclient11.DownloadFile(new Uri(download11), output11);
+            }
+
+            string download12 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/WindowsXP_logo.ico";
+            string output12 = Path.Combine(CreateFolder1, "WindowsXP_logo.ico");
+
+            using (WebClient webclient12 = new WebClient())
+            {
+                webclient12.DownloadFile(new Uri(download12), output12);
+            }
+
+            string download13 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/WindowsME_logo.ico";
+            string output13 = Path.Combine(CreateFolder1, "WindowsME_logo.ico");
+
+            using (WebClient webclient13 = new WebClient())
+            {
+                webclient13.DownloadFile(new Uri(download13), output13);
+            }
+
+            string download14 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows2000_logo.ico";
+            string output14 = Path.Combine(CreateFolder1, "Windows2000_logo.ico");
+
+            using (WebClient webclient14 = new WebClient())
+            {
+                webclient14.DownloadFile(new Uri(download14), output14);
+            }
+
+            string download15 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows98_logo.ico";
+            string output15 = Path.Combine(CreateFolder1, "Windows98_logo.ico");
+
+            using (WebClient webclient15 = new WebClient())
+            {
+                webclient15.DownloadFile(new Uri(download15), output15);
+            }
+
+            string download16 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows95_logo.ico";
+            string output16 = Path.Combine(CreateFolder1, "Windows95_logo.ico");
+
+            using (WebClient webclient16 = new WebClient())
+            {
+                webclient16.DownloadFile(new Uri(download16), output16);
+            }
+
+            string download17 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows3.1_logo.ico";
+            string output17 = Path.Combine(CreateFolder1, "Windows3.1_logo.ico");
+
+            using (WebClient webclient17 = new WebClient())
+            {
+                webclient17.DownloadFile(new Uri(download17), output17);
+            }
+
+            string download18 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows2_logo.ico";
+            string output18 = Path.Combine(CreateFolder1, "Windows2_logo.ico");
+
+            using (WebClient webclient18 = new WebClient())
+            {
+                webclient18.DownloadFile(new Uri(download18), output18);
+            }
+
+            string download19 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows_logo.ico";
+            string output19 = Path.Combine(CreateFolder1, "Windows_logo.ico");
+
+            using (WebClient webclient19 = new WebClient())
+            {
+                webclient19.DownloadFile(new Uri(download19), output19);
+            }
+
+            string download20 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/GetThatOSOther_logo.ico";
+            string output20 = Path.Combine(CreateFolder1, "GetThatOSOther_logo.ico");
+
+            using (WebClient webclient20 = new WebClient())
+            {
+                webclient20.DownloadFile(new Uri(download20), output20);
+            }
+
+            string download21 = "https://github.com/miniusbhater/GetThatOS/raw/main/GetThatOS%20Resources/LogosAndImages/Windows98953_logo.ico";
+            string output21 = Path.Combine(CreateFolder1, "Windows98953_logo.ico");
+
+            using (WebClient webclient20 = new WebClient())
+            {
+                webclient20.DownloadFile(new Uri(download20), output20);
+            }
+
+
+
+
+
+
+
+
+            label5.Text = ("Creating start menu shortcut");
+            string gosshortcut = "GetThatOS.lnk";
+            string target = @"C:\Program Files (x86)\GetThatOS\GetThatOS.exe";
+            string start = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu);
+            string shortcutpath = Path.Combine(start, "Programs", gosshortcut);           
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutpath);
+            shortcut.TargetPath = target;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(target);
+            shortcut.Save();
+
+
+                label5.Visible = false;
+                label6.Visible = true;
+                MessageBox.Show("Download complete!");
+            }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainInstaller_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
